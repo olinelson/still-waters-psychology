06b65c782581865b1e024d02f00ed95083d0a971
Oli Nelson <olivernelson@icloud.com>
2020-01-21 11:20:11 -0500

    Updates

---
+++ component---src-pages-404-js-9e210ae3105c59c8d4bd.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),r=n("Bl7J"),o=n("FA9A");t.default=function(){return l.a.createElement(r.a,null,l.a.createElement(o.c,{text:!0},l.a.createElement("h2",null,l.a.createElement("span",{role:"img","aria-label":"Forbidden sign"},"🚫")," ","Not found"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-9e210ae3105c59c8d4bd.js.map
