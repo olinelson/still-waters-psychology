8328cabdb3050d6cf35057c0bd2a13bebdcd46a9
Oli Nelson <olivernelson@icloud.com>
2020-01-29 17:50:33 -0500

    Updates

---
+++ component---src-pages-404-js-69ff76b19d2f4720a334.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),r=n("Bl7J"),o=n("Dzhk");t.default=function(){return l.a.createElement(r.a,null,l.a.createElement(o.d,{text:!0},l.a.createElement("h2",null,l.a.createElement("span",{role:"img","aria-label":"Forbidden sign"},"🚫")," ","Not found"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-69ff76b19d2f4720a334.js.map
